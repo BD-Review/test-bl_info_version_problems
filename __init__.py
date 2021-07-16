8ac59a665c5f9a7a5a715378d33012975df28ac1
BD-Review <84032165+BD-Review@users.noreply.github.com>
2021-07-16 14:49:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,14 +12,14 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name": "Test [NAME]",
+    "name": "Test BL Info Invalid Version",
     "author": "BD",
     "description": "",
     "blender": (2, 80, 0),
-    "version": (0, 0, 0),
+    "version": ("0", "Test", None),
     "location": "",
     "warning": "",
-    "endpoint_url": "https://raw.githubusercontent.com/BD-Review/Test_[NAME]/main/endpoint.json",
+    "endpoint_url": "https://raw.githubusercontent.com/BD-Review/test-bl_info_invalid_version/main/endpoint.json",
     "category": "Generic"
 }
 
